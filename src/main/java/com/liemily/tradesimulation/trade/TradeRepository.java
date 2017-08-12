3ef1947c6fcdf39a5aefe4aabb8ddd6c71bf7c4d
emily-li <emily.li@live.co.uk>
2017-08-12 22:27:24 +0100

    Creating TradeRepository

---
+++ src/main/java/com/liemily/tradesimulation/trade/TradeRepository.java
@@ -0,0 +1,10 @@
+package com.liemily.tradesimulation.trade;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by Emily Li on 12/08/2017.
+ */
+public interface TradeRepository extends JpaRepository<Trade, String> {
+
+}
