d2e258a8f680a266d9e255e2dcdb57c2b5972b76
emily-li <emily.li@live.co.uk>
2017-08-12 22:27:11 +0100

    Creating Trade domain object

---
+++ src/main/java/com/liemily/tradesimulation/trade/Trade.java
@@ -0,0 +1,78 @@
+package com.liemily.tradesimulation.trade;
+
+import javax.persistence.*;
+
+/**
+ * Created by Emily Li on 12/08/2017.
+ */
+@Entity
+public class Trade {
+    @Id
+    @GeneratedValue
+    private long orderId;
+    private String username;
+    private String stockSymbol;
+    private int volume;
+    @Enumerated(EnumType.STRING)
+    private TradeType tradeType;
+
+    // Default constructor required for Spring JPA
+    public Trade() {
+    }
+
+    public Trade(String username, String stockSymbol, int volume, TradeType tradeType) {
+        this.username = username;
+        this.stockSymbol = stockSymbol;
+        this.volume = volume;
+        this.tradeType = tradeType;
+    }
+
+    public long getOrderId() {
+        return orderId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getStockSymbol() {
+        return stockSymbol;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public TradeType getTradeType() {
+        return tradeType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Trade trade = (Trade) o;
+
+        if (orderId != trade.orderId) return false;
+        if (volume != trade.volume) return false;
+        if (username != null ? !username.equals(trade.username) : trade.username != null) return false;
+        if (stockSymbol != null ? !stockSymbol.equals(trade.stockSymbol) : trade.stockSymbol != null) return false;
+        return tradeType == trade.tradeType;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (orderId ^ (orderId >>> 32));
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (stockSymbol != null ? stockSymbol.hashCode() : 0);
+        result = 31 * result + volume;
+        result = 31 * result + (tradeType != null ? tradeType.hashCode() : 0);
+        return result;
+    }
+
+    public enum TradeType {
+        BUY,
+        SELL
+    }
+}
