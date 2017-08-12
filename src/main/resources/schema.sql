3ee60de11c7a8f57ebca656cdb7e3ed91bca41a2
emily-li <emily.li@live.co.uk>
2017-08-12 22:25:16 +0100

    Adding trade table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,16 @@
 DROP TABLE IF EXISTS `stock`;
+DROP TABLE IF EXISTS `trade`;
 
 CREATE TABLE IF NOT EXISTS `stock` (
     `symbol` VARCHAR(50)    NOT NULL PRIMARY KEY,
     `value`  DECIMAL(13,2)  NOT NULL,
     `volume` INT            NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `trade` (
+    `order_id`      BIGINT      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `username`      VARCHAR(50) NOT NULL,
+    `stock_symbol`  VARCHAR(50) NOT NULL,
+    `volume`        INT         NOT NULL,
+    `trade_type`    VARCHAR(50) NOT NULL
 );
