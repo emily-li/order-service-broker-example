e3550531c2b35b9c9ee00c2b2cad5f0bddf87c37
emily-li <emily.li@live.co.uk>
2017-08-13 21:50:54 +0100

    Adding add and remove AccountStock methods

--- src/main/java/com/liemily/tradesimulation/accountstock/AccountStockRepository.java
+++ src/main/java/com/liemily/tradesimulation/accountstock/AccountStockRepository.java
@@ -1,9 +1,18 @@
 package com.liemily.tradesimulation.accountstock;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * Created by Emily Li on 12/08/2017.
  */
 public interface AccountStockRepository extends JpaRepository<AccountStock, AccountStockId> {
+    @Modifying
+    @Query("UPDATE AccountStock SET volume = volume + ?3 WHERE username = ?1 AND stock_symbol = ?2")
+    int add(String username, String stockSymbol, int volume);
+
+    @Modifying
+    @Query("UPDATE AccountStock SET volume = volume - ?3 WHERE username = ?1 AND stock_symbol = ?2 AND volume - ?3 >= 0")
+    int remove(String username, String stockSymbol, int volume);
 }
