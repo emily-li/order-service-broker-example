90a98b84081528761a30e4117c2c85881083848e
emily-li <emily.li@live.co.uk>
2017-08-13 20:12:37 +0100

    Adding AccountStock to store information regarding an account's stocks

---
+++ src/main/java/com/liemily/tradesimulation/accountstock/AccountStock.java
@@ -0,0 +1,70 @@
+package com.liemily.tradesimulation.accountstock;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import java.io.Serializable;
+
+/**
+ * Created by Emily Li on 12/08/2017.
+ */
+@Entity
+@IdClass(AccountStockId.class)
+public class AccountStock implements Serializable {
+    @Id
+    private String username;
+    @Id
+    private String stockSymbol;
+    private int volume;
+
+    // Required for Spring JPA
+    private AccountStock() {
+    }
+
+    public AccountStock(String username, String stockSymbol, int volume) {
+        this.username = username;
+        this.stockSymbol = stockSymbol;
+        this.volume = volume;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getStockSymbol() {
+        return stockSymbol;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    @Override
+    public String toString() {
+        return "AccountStock{" +
+                "username='" + username + '\'' +
+                ", stockSymbol='" + stockSymbol + '\'' +
+                ", volume=" + volume +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AccountStock that = (AccountStock) o;
+
+        if (volume != that.volume) return false;
+        if (username != null ? !username.equals(that.username) : that.username != null) return false;
+        return stockSymbol != null ? stockSymbol.equals(that.stockSymbol) : that.stockSymbol == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username != null ? username.hashCode() : 0;
+        result = 31 * result + (stockSymbol != null ? stockSymbol.hashCode() : 0);
+        result = 31 * result + volume;
+        return result;
+    }
+}
---
+++ src/main/java/com/liemily/tradesimulation/accountstock/AccountStockId.java
@@ -0,0 +1,56 @@
+package com.liemily.tradesimulation.accountstock;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+/**
+ * Created by Emily Li on 13/08/2017.
+ */
+@Embeddable
+public class AccountStockId implements Serializable {
+    private String username;
+    private String stockSymbol;
+
+    // Required for Spring JPA
+    private AccountStockId() {
+    }
+
+    AccountStockId(String username, String stockSymbol) {
+        this.username = username;
+        this.stockSymbol = stockSymbol;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getStockSymbol() {
+        return stockSymbol;
+    }
+
+    @Override
+    public String toString() {
+        return "AccountStockId{" +
+                "username='" + username + '\'' +
+                ", stockSymbol='" + stockSymbol + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AccountStockId that = (AccountStockId) o;
+
+        if (username != null ? !username.equals(that.username) : that.username != null) return false;
+        return stockSymbol != null ? stockSymbol.equals(that.stockSymbol) : that.stockSymbol == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username != null ? username.hashCode() : 0;
+        result = 31 * result + (stockSymbol != null ? stockSymbol.hashCode() : 0);
+        return result;
+    }
+}
