40ab66d284c719636f95f47bfb81383222b06ab2
emily-li <emily.li@live.co.uk>
2017-08-13 20:12:53 +0100

    Adding account_stock table to store information regarding an account's stocks

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,20 +3,28 @@ DROP TABLE IF EXISTS `trade`;
 DROP TABLE IF EXISTS `account`;
 
 CREATE TABLE IF NOT EXISTS `stock` (
-    `symbol` VARCHAR(50)    NOT NULL PRIMARY KEY,
-    `value`  DECIMAL(65,2)  NOT NULL,
-    `volume` INT            NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS `trade` (
-    `order_id`      BIGINT      NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `username`      VARCHAR(50) NOT NULL,
-    `stock_symbol`  VARCHAR(50) NOT NULL,
-    `volume`        INT         NOT NULL,
-    `trade_type`    VARCHAR(50) NOT NULL
+    `symbol`        VARCHAR(50)     NOT NULL PRIMARY KEY,
+    `value`         DECIMAL(65,2)   NOT NULL,
+    `volume`        INT             NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `account` (
-    `username`      VARCHAR(50)     NOT NULL,
+    `username`      VARCHAR(50)     NOT NULL PRIMARY KEY,
     `credits`       DECIMAL(65,2)   NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `account_stock` (
+    `username`      VARCHAR(50)     NOT NULL,
+    `stock_symbol`  VARCHAR(50)     NOT NULL,
+    `volume`        INT             NOT NULL,
+
+    PRIMARY KEY (username, stock_symbol)
+);
+
+CREATE TABLE IF NOT EXISTS `trade` (
+    `trade_id`      BIGINT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `username`      VARCHAR(50)     NOT NULL,
+    `stock_symbol`  VARCHAR(50)     NOT NULL,
+    `volume`        INT             NOT NULL,
+    `trade_type`    VARCHAR(50)     NOT NULL
 );
