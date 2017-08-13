2d36b3d71e594260b4e8d8a38319dee6e4249172
emily-li <emily.li@live.co.uk>
2017-08-13 20:15:59 +0100

    Fixing Id

--- src/main/java/com/liemily/tradesimulation/trade/TradeRepository.java
+++ src/main/java/com/liemily/tradesimulation/trade/TradeRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * Created by Emily Li on 12/08/2017.
  */
-public interface TradeRepository extends JpaRepository<Trade, String> {
+public interface TradeRepository extends JpaRepository<Trade, Long> {
 
 }
